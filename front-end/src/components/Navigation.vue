78f3dd45e5e06fb52597e10661d9c8bf4afd60d4
Chase Carter <ccarter98@gmail.com>
2020-04-01 19:33:32 -0400

    nginx

---
+++ front-end/src/components/Navigation.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+    <v-navigation-drawer v-model="drawer" app mini-variant >
+      <v-list dense nav>
+        <v-list-item :to="item.link" v-for="item in headers" :key="item.title">
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#002939" dark>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Help Desk</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field hide-details prepend-icon="mdi-magnify" single-line placeholder="Search"></v-text-field>
+        <v-spacer></v-spacer>
+        <v-speed-dial
+        direction="bottom"
+        :open-on-hover="true"
+      >
+        <template v-slot:activator>
+          <v-avatar
+            color="blue"
+            dark
+            fab
+          >{{initials}}
+          </v-avatar>
+        </template>
+        <v-btn dark small fab color="blue"><v-icon>mdi-lightbulb</v-icon></v-btn>
+        <v-btn @click="logout()" fab dark small color="red">
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+      </v-speed-dial>
+      </v-app-bar>
+    </div>
+</template>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script>
+import axios from 'axios';
+export default {
+    name:'Navigation',
+    data: ()=>({
+        headers: [],
+        initials: "",
+        drawer: false
+    }),
+    methods: {
+      logout: () => {
+        axios.post('http://localhost:5000/api/session/logout').then().catch(err=>{
+          console.log(err)
+        })
+      },
+      getInitials: function(){
+        axios.get('http://localhost:5000/api/users').then(response=>{
+          this.initials = response.data.fname.charAt(0).toUpperCase()+response.data.lname.charAt(0).toUpperCase()
+        }).catch(err=>{
+          console.log(err)
+        })
+      },
+      getRoles: function(){
+        axios.get('http://localhost:5000/api/roles').then(response=>{
+        this.headers = response.data.pages;
+      }).catch(err=>{
+          console.log(err)
+        })
+      }
+    },
+    mounted() {
+      this.getRoles()
+      this.getInitials()
+    },
+}
+</script>
---
+++ front-end/src/components/TicketOverlay.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-dialog max-width="80%" @click:outside="showTicket = !showTicket" :value='showTicket'>
+        <v-card full-width>
+        <v-card-title></v-card-title>
+        <v-container>
+            <v-row>
+            <v-col v-show="activeTicket.ticketNumber">
+                <h2>Ticket #{{activeTicket.ticketNumber}}</h2>
+            </v-col>
+            <v-col>
+              <v-checkbox v-model="activeTicket.project" label="Project"></v-checkbox>
+            </v-col>
+            </v-row>
+            <v-row>
+            <v-col v-show="activeTicket.createdAt">
+                <h3>Created: </h3><p>{{activeTicket.createdAt}}</p>
+                </v-col>
+                <v-col v-show="activeTicket.closedAt">
+                <h3>Closed: </h3><p>{{activeTicket.closedAt}}</p>
+                </v-col>
+            </v-row>
+                          <v-row>
+                <v-col>
+                  <v-text-field label="Contact" v-model="activeTicket.callerName"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field label="Subject" v-model="activeTicket.subject"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-textarea label="Problem" v-model="activeTicket.problem"></v-textarea>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-textarea label="Solution" v-model="activeTicket.solution"></v-textarea>
+                </v-col>
+              </v-row>
+              <!--
+                <v-row v-show="activeTicket.nextSteps[0]">
+                  <v-col>
+                  <h2>Notes</h2>
+                  </v-col>
+                </v-row>
+              <v-row>
+                <v-col>
+                    <v-textarea v-for="step in activeTicket.nextSteps" :key="step.timestamp" v-model="step.text" :label="step.timestamp"></v-textarea>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                <v-btn>Add Note</v-btn>
+                </v-col>
+              </v-row>
+              -->
+            <v-row>
+            <v-col align='right'>
+                <v-btn class="ma-1" v-show="activeTicket.ticketNumber" @click="closeTicket()">Close Ticket</v-btn>
+                <v-btn class="ma-1" color="primary" @click="updateTicket()">Save</v-btn>
+            </v-col>
+            </v-row>
+        </v-container>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'TicketOverlay',
+    data: ()=>({
+    }),
+    props: ['activeTicket','showTicket'],
+    methods:{
+      updateTicket: function(){
+        if(this.activeTicket.ticketNumber == undefined){
+          axios.post('http://localhost:5000/api/tickets/',this.activeTicket)
+        }
+        else{
+        axios
+          .put('http://localhost:5000/api/tickets/'+this.activeTicket.ticketNumber,this.activeTicket)
+          .then(this.showTicket = false)
+        }
+      },
+      closeTicket: function(){
+        console.log(this.activeTicket)
+        var time = new Date()
+        this.activeTicket.closedAt = time
+        this.updateTicket()
+      }
+    },
+    mounted: function (){
+        axios.defaults.withCredentials = true
+    }
+}
+</script>
